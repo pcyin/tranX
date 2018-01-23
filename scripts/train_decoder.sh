b3882d8f5d1e8ddd1d810f25325d9e869076992f
Pengcheng Yin <pengchey@tir.lti.cs.cmu.edu>
2018-01-23 00:28:38 -0500

    added more

---
+++ scripts/train_decoder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+vocab="vocab.freq5.bin"
+dropout=0.2
+hidden_size=256
+embed_size=128
+ptrnet_hidden_dim=32
+lr_decay=0.5
+lstm='lstm'
+model_name=model.sup.decoder.${lstm}.hidden_size${hidden_size}.embed${embed_size}.dropout${dropout}.ptr_hidden${ptrnet_hidden_dim}.lr_decay${lr_decay}.${vocab}
+
+python exp.py \
+    --cuda \
+    --mode train \
+    --batch_size 10 \
+    --train_file ../data/django/train.bin \
+    --dev_file ../data/django/dev.bin \
+    --vocab ../data/django/${vocab} \
+    --lstm ${lstm} \
+    --hidden_size ${hidden_size} \
+    --embed_size ${embed_size} \
+    --ptrnet_hidden_dim ${ptrnet_hidden_dim} \
+    --dropout ${dropout} \
+    --patience 5 \
+    --max_num_trial 5 \
+    --lr_decay ${lr_decay} \
+    --beam_size ${beam_size} \
+    --log_every 50 \
+    --save_to saved_models/${model_name} 2>logs/${model_name}.log
