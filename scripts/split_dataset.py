233aae102d9e603642143de05b92b3a4c32f736c
Pengcheng Yin <pcyin1992@gmail.com>
2018-01-23 01:25:38 -0500

    split dataset

---
+++ scripts/split_dataset.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+from __future__ import print_function
+
+import sys, os, cPickle as pickle
+import numpy as np
+np.random.seed(0)
+
+from exp import *
+
+if __name__ == '__main__':
+    full_train_file = sys.argv[1]
+    full_train_set = pickle.load(open(full_train_file))
+
+    splits = [3000, 5000, 8000, 10000]
+    np.random.shuffle(full_train_set)
+
+    fname, ext = os.path.splitext(full_train_file)
+
+    for split in splits:
+        sup_examples = full_train_set[:split]
+        remaining_examples = full_train_set[split:]
+
+        pickle.dump(sup_examples, open(fname + '.%d' % split + ext, 'wb'))
+        pickle.dump(remaining_examples, open(fname + '.%d.remaining' % split + ext, 'wb'))
---
+++ scripts/split_dataset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=. python scripts/split_dataset.py data/django/train.bin
