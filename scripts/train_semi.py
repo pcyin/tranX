7dea3182a437707e9d241b018955510f5241d5b6
Pengcheng Yin <pengchey@tir.lti.cs.cmu.edu>
2018-01-23 22:27:01 -0500

    update

--- scripts/train_semi.py
+++ scripts/train_semi.py
@@ -4,21 +4,21 @@ from sh import git, sbatch
 import uuid
 import time
 
-train_data = "full"
-unlabeled_data = ""
+train_data = "train.5000.bin"
+unlabeled_data = "train.5000.remaining.bin"
 lr_decay = 0.5
 unsup_loss_weight = 0.1
-encoder = ""
-decoder = ""
+encoder = "model.sup.lstm_with_dropout.hidden_size256.embed128.action_embed128.field_embed64.type_embed64.dropout0.2.ptr_hidden32.lr_decay0.5.beam_size15.vocab.freq5.bin.train.5000.bin.bin"
+decoder = "model.sup.decoder.lstm.hidden_size256.embed128.dropout0.2.ptr_hidden32.lr_decay0.5.vocab.freq5.bin.train.5000.bin.bin"
 exp_id = time.strftime("%Y%m%d-%H%M%S") + '-' + str(uuid.uuid4())[:4]
 
-job_name = 'model.semisup.label_{train_data}.unlabeled_{unlabeled_data}.lr_decay{lr_decay}' \
-           '.unsup_w{unsup_loss_weight}.encoder_{encoder}.decoder_{decoder}.exp_id'.format(train_data=train_data,
+job_name = 'model.semisup.label_{train_data}.unlabeled_{unlabeled_data}.lr_d{lr_decay}' \
+           '.unsup_w{unsup_loss_weight}.enc_{encoder}.dec_{decoder}.{exp_id}'.format(train_data=train_data,
                                                                                            unlabeled_data=unlabeled_data,
                                                                                            lr_decay=lr_decay,
                                                                                            unsup_loss_weight=0.1,
-                                                                                           encoder=encoder[:10],
-                                                                                           decoder=decoder[:10],
+                                                                                           encoder=encoder[:20],
+                                                                                           decoder=decoder[:20],
                                                                                            exp_id=exp_id)
 job_script = '.job_scripts/%s.sh' % job_name
 with open(job_script, 'w') as f:
@@ -28,13 +28,14 @@ echo "{job_name}" > logs/{job_name}.log
 
 python exp.py \
     --cuda \
-    --mode train \
+    --mode train_semi \
     --batch_size 10 \
     --train_file ../data/django/{train_data} \
     --unlabeled_file ../data/django/{unlabeled_data} \
     --dev_file ../data/django/dev.bin \
     --load_model saved_models/{encoder} \
     --load_decoder saved_models/{decoder} \
+    --unsup_loss_weight {unsup_loss_weight} \
     --patience 5 \
     --max_num_trial 3 \
     --lr_decay {lr_decay} \
@@ -50,12 +51,13 @@ python exp.py \
 
 """.format(train_data=train_data, unlabeled_data=unlabeled_data,
            encoder=encoder, decoder=decoder,
+           unsup_loss_weight=unsup_loss_weight,
            lr_decay=lr_decay,
            job_name=job_name))
 
 cmd = sbatch('--gres', 'gpu:1',
              '--job-name', job_name,
-             '--mem', 25000,  # memory
+             '--mem', 15000,  # memory
              '--cpus-per-task', 8,  # number of cpus
              '--time', 0,  # wait time: unlimited
              '--output', 'logs/%s.out' % job_name,  # redirect stdout to file
--- scripts/train_semi.sh
+++ scripts/train_semi.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-train_data="full"
-unlabeled_data=""
+train_data="train.5000.bin"
+unlabeled_data="train.5000.remaining.bin"
 lr_decay=0.5
-encoder=""
-decoder=""
-model_name=model.semisup.encoder${encoder}.decoder${decoder}.lr_decay${lr_decay}.label_data${train_data}.unlabeled_data${unlabeled_data}
+encoder="model.sup.lstm_with_dropout.hidden_size256.embed128.action_embed128.field_embed64.type_embed64.dropout0.2.ptr_hidden32.lr_decay0.5.beam_size15.vocab.freq5.bin.train.5000.bin.bin"
+decoder="model.sup.decoder.lstm.hidden_size256.embed128.dropout0.2.ptr_hidden32.lr_decay0.5.vocab.freq5.bin.train.5000.bin.bin"
+unsup_loss_weight=0.1
+model_name=model.semisup.${encoder}.unlabeled_${unlabeled_data}.unsup_loss_weight_${unsup_loss_weight}
 
 python exp.py \
     --cuda \
-    --mode train \
+    --mode train_semi \
     --batch_size 10 \
-    --train_file data/django/${train_data} \
-    --unlabeled_file data/django/${unlabeled_data} \
-    --dev_file data/django/dev.bin \
+    --train_file ../data/django/${train_data} \
+    --unlabeled_file ../data/django/${unlabeled_data} \
+    --dev_file ../data/django/dev.bin \
     --load_model saved_models/${encoder} \
     --load_decoder saved_models/${decoder} \
+    --unsup_loss_weight 0.1 \
     --patience 5 \
     --max_num_trial 3 \
     --lr_decay ${lr_decay} \
