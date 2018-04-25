f38dc0d586e482f02bf4722956c984d97ee1558c
Pengcheng Yin <pcyin@cs.cmu.edu>
2018-04-24 22:31:19 -0400

    update args. bug fix

--- exp.py
+++ exp.py
@@ -26,87 +26,94 @@ from model.reconstruction_model import Reconstructor
 from model.struct_vae import StructVAE, StructVAE_LMBaseline, StructVAE_SrcLmAndLinearBaseline
 from model.utils import GloveHelper
 
-def init_config():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', default=5783287, type=int, help='random seed')
-    parser.add_argument('--cuda', action='store_true', default=False, help='use gpu')
-    parser.add_argument('--lang', choices=['python', 'lambda_dcs', 'wikisql', 'prolog'], default='python')
-    parser.add_argument('--mode', choices=['train', 'self_train', 'train_decoder', 'train_semi', 'log_semi', 'test', 'sample'], default='train', help='run mode')
 
-    parser.add_argument('--lstm', choices=['lstm', 'lstm_with_dropout', 'parent_feed'], default='lstm')
+def init_arg_parser():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--seed', default=5783287, type=int, help='random seed')
+    arg_parser.add_argument('--cuda', action='store_true', default=False, help='use gpu')
+    arg_parser.add_argument('--lang', choices=['python', 'lambda_dcs', 'wikisql', 'prolog'], default='python')
+    arg_parser.add_argument('--mode', choices=['train', 'self_train', 'train_decoder', 'train_semi', 'log_semi', 'test',
+                                               'sample'], default='train', help='run mode')
 
-    parser.add_argument('--load_model', default=None, type=str, help='load a pre-trained model')
-    parser.add_argument('--glove_embed_path', default=None, type=str)
+    arg_parser.add_argument('--lstm', choices=['lstm', 'lstm_with_dropout', 'parent_feed'], default='lstm')
 
-    parser.add_argument('--batch_size', default=10, type=int, help='batch size')
-    parser.add_argument('--unsup_batch_size', default=10, type=int)
-    parser.add_argument('--beam_size', default=5, type=int, help='beam size for beam search')
-    parser.add_argument('--sample_size', default=5, type=int, help='sample size')
-    parser.add_argument('--embed_size', default=128, type=int, help='size of word embeddings')
-    parser.add_argument('--action_embed_size', default=128, type=int, help='size of word embeddings')
-    parser.add_argument('--field_embed_size', default=64, type=int, help='size of word embeddings')
-    parser.add_argument('--type_embed_size', default=64, type=int, help='size of word embeddings')
-    parser.add_argument('--ptrnet_hidden_dim', default=32, type=int)
-    parser.add_argument('--hidden_size', default=256, type=int, help='size of LSTM hidden states')
-    parser.add_argument('--dropout', default=0., type=float, help='dropout rate')
-    parser.add_argument('--decoder_word_dropout', default=0.3, type=float, help='word dropout on decoder')
-    parser.add_argument('--kl_anneal', default=False, action='store_true')
-    parser.add_argument('--alpha', default=0.1, type=float)
+    arg_parser.add_argument('--load_model', default=None, type=str, help='load a pre-trained model')
+    arg_parser.add_argument('--glove_embed_path', default=None, type=str)
+
+    arg_parser.add_argument('--batch_size', default=10, type=int, help='batch size')
+    arg_parser.add_argument('--unsup_batch_size', default=10, type=int)
+    arg_parser.add_argument('--beam_size', default=5, type=int, help='beam size for beam search')
+    arg_parser.add_argument('--sample_size', default=5, type=int, help='sample size')
+    arg_parser.add_argument('--embed_size', default=128, type=int, help='size of word embeddings')
+    arg_parser.add_argument('--action_embed_size', default=128, type=int, help='size of word embeddings')
+    arg_parser.add_argument('--field_embed_size', default=64, type=int, help='size of word embeddings')
+    arg_parser.add_argument('--type_embed_size', default=64, type=int, help='size of word embeddings')
+    arg_parser.add_argument('--ptrnet_hidden_dim', default=32, type=int)
+    arg_parser.add_argument('--hidden_size', default=256, type=int, help='size of LSTM hidden states')
+    arg_parser.add_argument('--dropout', default=0., type=float, help='dropout rate')
+    arg_parser.add_argument('--decoder_word_dropout', default=0.3, type=float, help='word dropout on decoder')
+    arg_parser.add_argument('--kl_anneal', default=False, action='store_true')
+    arg_parser.add_argument('--alpha', default=0.1, type=float)
 
     # parent information switch
-    parser.add_argument('--no_parent_production_embed', default=False, action='store_true')
-    parser.add_argument('--no_parent_field_embed', default=False, action='store_true')
-    parser.add_argument('--no_parent_field_type_embed', default=False, action='store_true')
-    parser.add_argument('--no_parent_state', default=False, action='store_true')
+    arg_parser.add_argument('--no_parent_production_embed', default=False, action='store_true')
+    arg_parser.add_argument('--no_parent_field_embed', default=False, action='store_true')
+    arg_parser.add_argument('--no_parent_field_type_embed', default=False, action='store_true')
+    arg_parser.add_argument('--no_parent_state', default=False, action='store_true')
 
-    parser.add_argument('--asdl_file', type=str)
-    parser.add_argument('--vocab', type=str, help='path of the serialized vocabulary')
-    parser.add_argument('--train_src', type=str, help='path to the training source file')
-    parser.add_argument('--unlabeled_file', type=str, help='path to the training source file')
-    parser.add_argument('--train_file', type=str, help='path to the training target file')
-    parser.add_argument('--dev_file', type=str, help='path to the dev source file')
-    parser.add_argument('--test_file', type=str, help='path to the test target file')
-    parser.add_argument('--prior_lm_path', type=str, help='path to the prior LM')
+    arg_parser.add_argument('--asdl_file', type=str)
+    arg_parser.add_argument('--vocab', type=str, help='path of the serialized vocabulary')
+    arg_parser.add_argument('--train_src', type=str, help='path to the training source file')
+    arg_parser.add_argument('--unlabeled_file', type=str, help='path to the training source file')
+    arg_parser.add_argument('--train_file', type=str, help='path to the training target file')
+    arg_parser.add_argument('--dev_file', type=str, help='path to the dev source file')
+    arg_parser.add_argument('--test_file', type=str, help='path to the test target file')
+    arg_parser.add_argument('--prior_lm_path', type=str, help='path to the prior LM')
 
     # self-training
-    parser.add_argument('--load_decode_results', default=None, type=str)
+    arg_parser.add_argument('--load_decode_results', default=None, type=str)
 
     # semi-supervised learning arguments
-    parser.add_argument('--load_decoder', default=None, type=str)
-    parser.add_argument('--load_src_lm', default=None, type=str)
+    arg_parser.add_argument('--load_decoder', default=None, type=str)
+    arg_parser.add_argument('--load_src_lm', default=None, type=str)
 
-    parser.add_argument('--baseline', choices=['mlp', 'src_lm', 'src_lm_and_linear'], default='mlp')
-    parser.add_argument('--prior', choices=['lstm', 'uniform'])
-    parser.add_argument('--load_prior', type=str, default=None)
-    parser.add_argument('--clip_learning_signal', type=float, default=None)
-    parser.add_argument('--begin_semisup_after_dev_acc', type=float, default=0., help='begin semi-supervised learning after'
-                                                                                    'we have reached certain dev performance')
+    arg_parser.add_argument('--baseline', choices=['mlp', 'src_lm', 'src_lm_and_linear'], default='mlp')
+    arg_parser.add_argument('--prior', choices=['lstm', 'uniform'])
+    arg_parser.add_argument('--load_prior', type=str, default=None)
+    arg_parser.add_argument('--clip_learning_signal', type=float, default=None)
+    arg_parser.add_argument('--begin_semisup_after_dev_acc', type=float, default=0.,
+                            help='begin semi-supervised learning after'
+                                 'we have reached certain dev performance')
 
-    parser.add_argument('--decode_max_time_step', default=100, type=int, help='maximum number of time steps used '
-                                                                              'in decoding and sampling')
-    parser.add_argument('--unsup_loss_weight', default=1., type=float, help='loss of unsupervised learning weight')
+    arg_parser.add_argument('--decode_max_time_step', default=100, type=int, help='maximum number of time steps used '
+                                                                                  'in decoding and sampling')
+    arg_parser.add_argument('--unsup_loss_weight', default=1., type=float, help='loss of unsupervised learning weight')
 
-    parser.add_argument('--valid_metric', default='sp_acc', choices=['nlg_bleu', 'sp_acc'],
-                        help='metric used for validation')
-    parser.add_argument('--log_every', default=10, type=int, help='every n iterations to log training statistics')
+    arg_parser.add_argument('--valid_metric', default='sp_acc', choices=['nlg_bleu', 'sp_acc'],
+                            help='metric used for validation')
+    arg_parser.add_argument('--log_every', default=10, type=int, help='every n iterations to log training statistics')
 
-    parser.add_argument('--save_to', default='model', type=str, help='save trained model to')
-    parser.add_argument('--save_decode_to', default=None, type=str, help='save decoding results to file')
-    parser.add_argument('--patience', default=5, type=int, help='training patience')
-    parser.add_argument('--max_num_trial', default=10, type=int)
-    parser.add_argument('--uniform_init', default=None, type=float,
-                        help='if specified, use uniform initialization for all parameters')
-    parser.add_argument('--clip_grad', default=5., type=float, help='clip gradients')
-    parser.add_argument('--max_epoch', default=-1, type=int, help='maximum number of training epoches')
-    parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--lr_decay', default=0.5, type=float,
-                        help='decay learning rate if the validation performance drops')
-    parser.add_argument('--lr_decay_after_epoch', default=5, type=int)
-    parser.add_argument('--reset_optimizer', action='store_true', default=False)
+    arg_parser.add_argument('--save_to', default='model', type=str, help='save trained model to')
+    arg_parser.add_argument('--save_decode_to', default=None, type=str, help='save decoding results to file')
+    arg_parser.add_argument('--patience', default=5, type=int, help='training patience')
+    arg_parser.add_argument('--max_num_trial', default=10, type=int)
+    arg_parser.add_argument('--uniform_init', default=None, type=float,
+                            help='if specified, use uniform initialization for all parameters')
+    arg_parser.add_argument('--clip_grad', default=5., type=float, help='clip gradients')
+    arg_parser.add_argument('--max_epoch', default=-1, type=int, help='maximum number of training epoches')
+    arg_parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+    arg_parser.add_argument('--lr_decay', default=0.5, type=float,
+                            help='decay learning rate if the validation performance drops')
+    arg_parser.add_argument('--lr_decay_after_epoch', default=5, type=int)
+    arg_parser.add_argument('--reset_optimizer', action='store_true', default=False)
 
-    parser.add_argument('--train_opt', default="reinforce", type=str, choices=['reinforce', 'st_gumbel'])
+    arg_parser.add_argument('--train_opt', default="reinforce", type=str, choices=['reinforce', 'st_gumbel'])
 
-    args = parser.parse_args()
+    return arg_parser
+
+
+def init_config():
+    args = arg_parser.parse_args()
 
     # seed the RNG
     torch.manual_seed(args.seed)
@@ -115,6 +122,13 @@ def init_config():
     np.random.seed(int(args.seed * 13 / 7))
 
     return args
+
+
+def update_args(args):
+    for action in arg_parser._actions:
+        if isinstance(action, argparse._StoreAction) or isinstance(action, argparse._StoreTrueAction) or isinstance(action, argparse._StoreFalseAction):
+            if not hasattr(args, action.dest):
+                setattr(args, action.dest, action.default)
 
 
 def get_parser_class(lang):
@@ -399,6 +413,8 @@ def self_training(args):
     saved_args.load_decode_results = args.load_decode_results
     args = saved_args
 
+    update_args(args)
+
     model = Parser(saved_args, vocab, transition_system)
     model.load_state_dict(saved_state)
 
@@ -542,6 +558,9 @@ def train_semi(args):
 
     transition_system = encoder_params['transition_system']
     encoder_params['args'].cuda = decoder_params['args'].cuda = args.cuda
+
+    update_args(encoder_params['args'])
+    update_args(decoder_params['args'])
 
     encoder = Parser(encoder_params['args'], encoder_params['vocab'], transition_system)
     encoder.load_state_dict(encoder_params['state_dict'])
@@ -939,6 +958,8 @@ def test(args):
     # set the correct domain from saved arg
     args.lang = saved_args.lang
 
+    update_args(saved_args)
+
     parser_cls = get_parser_class(saved_args.lang)
     parser = parser_cls(saved_args, vocab, transition_system)
 
@@ -955,6 +976,7 @@ def test(args):
 
 
 if __name__ == '__main__':
+    arg_parser = init_arg_parser()
     args = init_config()
     print(args, file=sys.stderr)
     if args.mode == 'train':
