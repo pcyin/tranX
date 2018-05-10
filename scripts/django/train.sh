fd3582543d12a557a134dccd0befbb7b7a22e809
Pengcheng Yin <pengchey@tir.lti.cs.cmu.edu>
2018-05-10 10:58:24 -0400

    script update

--- scripts/django/train.sh
+++ scripts/django/train.sh
@@ -2,18 +2,19 @@
 
 vocab="vocab.freq5.bin"
 train_file="train.bin"
-dropout=0.3
-hidden_size=256
+dropout=0.4
+hidden_size=300
 embed_size=128
 action_embed_size=128
 field_embed_size=64
 type_embed_size=64
 ptrnet_hidden_dim=32
+lr=0.001
 lr_decay=0.5
 beam_size=15
-lstm='parent_feed'  # lstm
-seed=1
-model_name=model.sup.django.${lstm}.hidden${hidden_size}.embed${embed_size}.action${action_embed_size}.field${field_embed_size}.type${type_embed_size}.dropout${dropout}.lr_decay${lr_decay}.beam_size${beam_size}.${vocab}.${train_file}.seed${seed}.w_prod_info
+lstm='lstm'  # lstm
+seed=0
+model_name=model.sup.django.${lstm}.hidden${hidden_size}.embed${embed_size}.action${action_embed_size}.field${field_embed_size}.type${type_embed_size}.dropout${dropout}.lr${lr}.lr_decay${lr_decay}.beam_size${beam_size}.${vocab}.${train_file}.glorot.par_state_w_field_embed.seed${seed}
 
 python exp.py \
     --cuda \
@@ -25,6 +26,8 @@ python exp.py \
     --dev_file data/django/dev.bin \
     --vocab data/django/${vocab} \
     --lstm ${lstm} \
+    --no_parent_field_type_embed \
+    --no_parent_production_embed \
     --hidden_size ${hidden_size} \
     --embed_size ${embed_size} \
     --action_embed_size ${action_embed_size} \
@@ -33,6 +36,8 @@ python exp.py \
     --dropout ${dropout} \
     --patience 5 \
     --max_num_trial 5 \
+    --glorot_init \
+    --lr ${lr} \
     --lr_decay ${lr_decay} \
     --beam_size ${beam_size} \
     --log_every 50 \
