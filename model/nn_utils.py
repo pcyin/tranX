9cf64d1b3ea9bac9e50a657545858d3f6d12212a
Pengcheng Yin <pcyin@cs.cmu.edu>
2018-10-27 18:36:41 -0400

    bug fix

--- model/nn_utils.py
+++ model/nn_utils.py
@@ -173,7 +173,6 @@ class LabelSmoothing(nn.Module):
         one_hot = torch.zeros((tgt_vocab_size,)).fill_(smoothing_value)
         for idx in ignore_indices:
             one_hot[idx] = 0.
-        one_hot = Variable(one_hot, requires_grad=False)
 
         self.confidence = 1.0 - smoothing
         self.register_buffer('one_hot', one_hot.unsqueeze(0))
@@ -181,7 +180,7 @@ class LabelSmoothing(nn.Module):
     def forward(self, model_prob, target):
         # (batch_size, *, tgt_vocab_size)
         dim = list(model_prob.size())[:-1] + [1]
-        true_dist = self.one_hot.repeat(*dim)
+        true_dist = Variable(self.one_hot, requires_grad=False).repeat(*dim)
         true_dist.scatter_(-1, target.unsqueeze(-1), self.confidence)
         # true_dist = model_prob.data.clone()
         # true_dist.fill_(self.smoothing / (model_prob.size(1) - 1))  # FIXME: no label smoothing for <pad> <s> and </s>
