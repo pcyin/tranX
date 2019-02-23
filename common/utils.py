b82f75987df5fb417c37ef5f544627f594741ff6
Pengcheng Yin <pcyin@cs.cmu.edu>
2019-02-22 20:53:39 -0500

    make --mode option required

--- common/utils.py
+++ common/utils.py
@@ -30,7 +30,7 @@ def init_arg_parser():
     arg_parser.add_argument('--lang', choices=['python', 'lambda_dcs', 'wikisql', 'prolog', 'python3'], default='python',
                             help='[Deprecated] language to parse. Deprecated, use --transition_system and --parser instead')
     arg_parser.add_argument('--asdl_file', type=str, help='Path to ASDL grammar specification')
-    arg_parser.add_argument('--mode', choices=['train', 'test', 'interactive'], default='train', help='Run mode')
+    arg_parser.add_argument('--mode', choices=['train', 'test', 'interactive'], required=True, help='Run mode')
 
     #### Modularized configuration ####
     arg_parser.add_argument('--parser', type=str, default='default_parser', required=False, help='name of parser class to load')
