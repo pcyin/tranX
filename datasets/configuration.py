592c52adfb7aaff45935f4406661f3c6905ee152
Pengcheng Yin <pcyin@cs.cmu.edu>
2019-02-22 19:30:16 -0500

    remove unused files

--- datasets/configuration.py
+++
@@ -1,8 +0,0 @@
-from collections import OrderedDict
-
-"""
-A global configuration dictionary that maps from dataset to some fixed config details (e.g., evaluator)
-
-TODO: use a better configuration strategy!
-"""
-dataset_config = OrderedDict()
