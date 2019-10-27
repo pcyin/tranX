72bca688b11e9a4c0a4c70d9b8d14abd947bbe95
Frank Xu <frankxu2004@gmail.com>
2019-10-27 06:38:05 -0400

    add reranking feature training code

--- scripts/conala/train_para.sh
+++ scripts/conala/train_para.sh
@@ -2,10 +2,11 @@
 set -e
 
 seed=0
-mined_num=$1
-vocab="data/conala/vocab.src_freq3.code_freq3.mined_${mined_num}.bin"
-train_file="data/conala/train.mined_${mined_num}.bin"
+vocab="data/conala/vocab.src_freq3.code_freq3.bin"
+train_file="data/conala/train.bin"
 dev_file="data/conala/dev.bin"
+train_decode_file="decodes/conala/conala.lstm.hidden256.embed128.action128.field64.type64.dr0.3.lr0.001.lr_de0.5.lr_da15.beam15.vocab.src_freq3.code_freq3.mined_0.bin.train.mined_0.bin.glorot.par_state.seed0.bin.train.bin.decode"
+dev_decode_file="decodes/conala/conala.lstm.hidden256.embed128.action128.field64.type64.dr0.3.lr0.001.lr_de0.5.lr_da15.beam15.vocab.src_freq3.code_freq3.mined_0.bin.train.mined_0.bin.glorot.par_state.seed0.bin.dev.bin.decode"
 dropout=0.3
 hidden_size=256
 embed_size=128
@@ -14,12 +15,12 @@ field_embed_size=64
 type_embed_size=64
 lr=0.001
 lr_decay=0.5
-batch_size=64
+batch_size=16
 max_epoch=80
 beam_size=15
 lstm='lstm'  # lstm
 lr_decay_after_epoch=15
-model_name=conala.${lstm}.hidden${hidden_size}.embed${embed_size}.action${action_embed_size}.field${field_embed_size}.type${type_embed_size}.dr${dropout}.lr${lr}.lr_de${lr_decay}.lr_da${lr_decay_after_epoch}.beam${beam_size}.$(basename ${vocab}).$(basename ${train_file}).glorot.par_state.seed${seed}
+model_name=para.conala.hidden${hidden_size}.embed${embed_size}.dr${dropout}.lr${lr}.seed${seed}
 
 echo "**** Writing results to logs/conala/${model_name}.log ****"
 mkdir -p logs/conala
@@ -28,13 +29,15 @@ echo commit hash: `git rev-parse HEAD` > logs/conala/${model_name}.log
 python -u exp.py \
     --cuda \
     --seed ${seed} \
-    --mode train \
+    --mode train_paraphrase_identifier \
     --batch_size ${batch_size} \
     --evaluator conala_evaluator \
     --asdl_file asdl/lang/py3/py3_asdl.simplified.txt \
     --transition_system python3 \
     --train_file ${train_file} \
     --dev_file ${dev_file} \
+    --train_decode_file ${train_decode_file} \
+    --dev_decode_file ${dev_decode_file} \
     --vocab ${vocab} \
     --lstm ${lstm} \
     --no_parent_field_type_embed \
@@ -56,4 +59,4 @@ python -u exp.py \
     --log_every 50 \
     --save_to saved_models/conala/${model_name} 2>&1 | tee logs/conala/${model_name}.log
 
-. scripts/conala/test.sh saved_models/conala/${model_name}.bin 2>&1 | tee -a logs/conala/${model_name}.log
+#. scripts/conala/test.sh saved_models/conala/${model_name}.bin 2>&1 | tee -a logs/conala/${model_name}.log
