94d3dd37c8ff5b12cb25ea1e5a2b508b0328fa85
removedporn <86824510+removedporn@users.noreply.github.com>
2022-11-12 08:06:54 +0800

    Delete asdl directory

--- asdl/utils.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-import re
-
-
-def remove_comment(text):
-    text = re.sub(re.compile("#.*"), "", text)
-    text = '\n'.join(filter(lambda x: x, text.split('\n')))
-
-    return text
